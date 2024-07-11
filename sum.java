62d61ec4128353548386eda08ceb3cf9119f6b15
nrk8008 <nrk8008@gmail.com>
2024-07-11 23:03:28 +0530

    adding sum.java file

---
+++ sum.java
@@ -0,0 +1,9 @@
+public class sum{
+
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 5;
+        int sum = num1+num2;
+        System.out.println("Sum = "+ sum);
+    }
+}
